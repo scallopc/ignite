0a70247983ccf788ac4245ea8c894622460c3bf6
Jaque <j.costa@dino.com.br>
2022-10-17 20:38:14 -0300

    storybook deployer

--- src/components/checkbox/checkbox.stories.tsx
+++ src/components/checkbox/checkbox.stories.tsx
@@ -1,4 +1,5 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from "@storybook/react";
+import { Text } from "../text/Text";
 import { Checkbox, CheckboxProps } from "./checkbox";
 
 export default {
@@ -6,6 +7,16 @@ export default {
   component: Checkbox,
   args: {},
   argTypes: {},
+  decorators: [
+    (Story) => {
+      return (
+      <div className="flex items-center gap-2">
+        {Story()} 
+      <Text size="sm"> Accept </Text>
+      </div>
+      );
+    },
+  ],
 } as Meta<CheckboxProps>;
 
 export const Default: StoryObj<CheckboxProps> = {};
